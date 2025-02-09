886a6de692505300b460bfcb139137072af28551
Ashwani <ashwani90125@gmail.com>
2025-02-10 00:14:46 +0530

    Work on setup file syst

--- python_requests.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-file_path = "invoice.pdf"
-url = "http://localhost:8000/extract/"
-
-with open(file_path, "rb") as f:
-    response = requests.post(url, files={"file": f})
-
-print(response.json())
